ea452e4f38828126178ad966d15d72e129ed6992
Alice <alicepsouzaba1998@gmail.com>
2021-03-16 18:41:36 -0300

    form page

--- src/pages/FormPage/index.tsx
+++ src/pages/FormPage/index.tsx
@@ -1,10 +1,48 @@
 import React from 'react';
+import AddButton from '../../components/AddButton';
+import InputSelect from '../../components/InputSelect';
+import InputText from '../../components/InputText';
+import QuestionCard from '../../components/QuestionCard';
+import FinalButtons from '../../components/FinalButtons';
+import Difficulty from '../../components/Difficulty';
+import { FormContainer, TopScreen, DescriptionContainer, QuestionContainer, FooterForm } from './styles';
 
 const FormPage: React.FC = () => {
   return (
-    <div>
-      
-    </div>
+    <FormContainer>
+      <TopScreen>
+        <div>
+          <InputText
+            label="Nome"
+            placeholder="Ex: Gramática"
+          />
+          <InputText
+            label="Código BNCC"
+            placeholder="Ex: 87H93O0"
+          />
+        </div>
+        <InputSelect />
+        <DescriptionContainer>
+          <label htmlFor="description">Descrição</label>
+          <textarea id="description" placeholder="Ex: Essa atividade está sendo..."></textarea>
+        </DescriptionContainer>
+      </TopScreen>
+      <QuestionContainer>
+        <QuestionCard
+          nameQuestion="Questão 1"
+          question="Marque a alternativa que apresenta equívoco de escolha lexical:"
+        />
+        <QuestionCard
+          nameQuestion="Questão 2"
+          question="Marque a alternativa que apresenta equívoco de escolha lexical:"
+        />
+        <AddButton />
+      </QuestionContainer>
+      <FooterForm>
+        <Difficulty />
+        <FinalButtons />
+      </FooterForm>
+    </FormContainer>
   );
 }
 
--- src/pages/FormPage/styles.ts
+++ src/pages/FormPage/styles.ts
@@ -1,1 +1,46 @@
 import styled from 'styled-components';
+
+export const FormContainer = styled.main`
+  background: #f2fcff;
+  font-family: Arial, Helvetica, sans-serif;
+  height: 100vh;
+  padding: 40px 50px 0 20px;
+`
+
+export const TopScreen = styled.div`
+  display: flex;
+`
+
+export const DescriptionContainer = styled.div`
+  margin-left: 15px;
+  label {
+    padding: 0 0 8px 10px;
+    font-size: 90%;
+    font-weight: 600;
+    color: #717988;
+  }
+  textarea {
+    display: flex;
+    flex-direction: column;
+    background: #f1f4f5;
+    width: 380px;
+    height: 82px;
+    padding-left: 10px;
+    border-radius: 5px;
+    border: 1px solid #d2d4d4;
+    margin-bottom: 12px;
+    outline: none;
+  }
+`
+
+export const QuestionContainer = styled.div`
+  display: flex;
+  margin-top: 30px;
+`
+
+export const FooterForm = styled.div`
+  margin-top: 40px;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+`
