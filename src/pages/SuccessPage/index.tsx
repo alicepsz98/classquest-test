e6ef33c9fa50a4a74ec76b4a4be7ea085ff971c3
Alice <alicepsouzaba1998@gmail.com>
2021-03-16 18:35:57 -0300

    success page

--- src/pages/SuccessPage/index.tsx
+++ src/pages/SuccessPage/index.tsx
@@ -1,10 +1,18 @@
 import React from 'react';
+import { useHistory } from 'react-router-dom';
+import logoClass from '../../assets/logo.png';
+import { goBack } from '../../router/Coordinator';
+import { SuccessContainer } from './styles';
 
 const SuccessPage: React.FC = () => {
+  const history = useHistory()
+
   return (
-    <div>
-      
-    </div>
+    <SuccessContainer>
+      <h1>Cadastro realizado com sucesso!</h1>
+      <img src={logoClass} alt=""/>
+      <h3 onClick={() => goBack(history)}><u>Voltar para tela de Cadastro</u></h3>
+    </SuccessContainer>
   );
 }
 
--- src/pages/SuccessPage/styles.ts
+++ src/pages/SuccessPage/styles.ts
@@ -1,1 +1,21 @@
 import styled from 'styled-components';
+
+export const SuccessContainer = styled.div`
+  text-align: center;
+  font-family: Arial, Helvetica, sans-serif;
+  width: 100%;
+  height: 100vh;
+  padding-top: 120px;
+  background: #f2fcff;
+  h1 {
+    font-size: 250%;
+    color: #717988;
+  }
+  img {
+    margin: 50px 0;
+  }
+  h3 {
+    cursor: pointer;
+    color: green;
+  }
+`
