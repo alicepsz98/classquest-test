ee6afe52651961ed80f534ab6439bb99d4c4bfe4
Alice <alicepsouzaba1998@gmail.com>
2021-03-16 11:55:33 -0300

    selection inputs

--- src/components/InputSelect/index.tsx
+++ src/components/InputSelect/index.tsx
@@ -1,10 +1,27 @@
 import React from 'react';
+import { InputContainer } from './styles';
 
 const InputSelect: React.FC = () => {
   return (
-    <div>
-      
-    </div>
+    <InputContainer>
+      <label htmlFor="subject">Matéria</label>
+      <select id="subject">
+        <option>Português</option>
+        <option>Matemática</option>
+        <option>Biologia</option>
+        <option>Geografia</option>
+        <option>Inglês</option>
+        <option>Espanhol</option>
+      </select>
+      <label htmlFor="class">Add na turma</label>
+      <select id="class">
+        <option>1º ano A</option>
+        <option>1º ano B</option>
+        <option>2º ano A</option>
+        <option>3º ano C</option>
+        <option>3º ano D</option>
+      </select>
+    </InputContainer>
   );
 }
 
--- src/components/InputSelect/styles.ts
+++ src/components/InputSelect/styles.ts
@@ -1,1 +1,25 @@
 import styled from 'styled-components';
+
+export const InputContainer = styled.div`
+  margin-left: 20px;
+  label {
+    padding: 5px 0 8px 10px;
+    font-size: 90%;
+    font-weight: 600;
+    color: #717988;
+  }
+  select {
+    color: #717988;
+    display: flex;
+    flex-direction: column;
+    background: #f1f4f5;
+    width: 160px;
+    height: 29px;
+    padding: 5px 5px 0 10px;
+    border-radius: 5px;
+    border: 1px solid #d2d4d4;
+    margin-bottom: 12px;
+    outline: none;
+    vertical-align: center;
+  }
+`
